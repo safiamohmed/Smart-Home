8d20441cff85933aefb1b3074b19340f08a7e6f9
Safia Mohamed <103541091+safiamohmed@users.noreply.github.com>
2025-05-01 18:30:53 +0300

    Add files via upload

---
+++ Smart Home/Slave_Home/main_confgration.h
@@ -0,0 +1,15 @@
+/*
+ * main_confgration.h
+ *
+ *  Created on: Sep 3, 2023
+ *      Author: admin
+ */
+
+#ifndef MAIN_CONFGRATION_H_
+#define MAIN_CONFGRATION_H_
+
+#define LED_Status 1
+
+
+
+#endif /* MAIN_CONFGRATION_H_ */
